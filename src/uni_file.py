eb6a696c4723daab8b67bf909a6912af54e1522b
unknown <yrezehi@gmail.com>
2023-11-06 19:51:10 -0800

    setup: unifile

---
+++ src/uni_file.py
@@ -0,0 +1,16 @@
+import os.path
+
+class UniFile():
+
+    def read(file_path):
+        if not os.path.isfile(file_path):
+          raise Exception("File does not exists " + file_path)
+
+    def write():
+        return
+    
+    def dump():
+        return
+
+    def remove():
+        return
